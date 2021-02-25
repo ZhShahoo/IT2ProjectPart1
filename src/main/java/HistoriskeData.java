3b878775ca7ed3da313be82b0731d1647a0040fc
ZAkir Hussain Shahoo <s194054@student.dtu.dk>
2021-02-25 15:59:18 +0100

    Initial commit

---
+++ src/main/java/HistoriskeData.java
@@ -0,0 +1,8 @@
+public class HistoriskeData {
+
+
+    public static void EgenData(){
+
+
+    }
+}
---
+++ src/main/java/Login.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Log ind📍");
+        stage.show();
+
+    }
+}
